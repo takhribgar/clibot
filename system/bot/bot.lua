4f16a8e5785fb8c96ff496e8607cd7271f92b987
takhribgar <amirbeh200@gmail.com>
2016-09-18 23:27:34 -0400

    Update bot.lua

--- system/bot/bot.lua
+++ system/bot/bot.lua
@@ -234,8 +234,8 @@ function create_config( )
     "tools",
     "filter"
     },
-    vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    vip_users = {259781677}, --vip users
+    sudo_users = {259781677},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'system/data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
